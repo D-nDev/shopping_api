3763573b037ecaddd2b8cae8168f422f539f3f75
D-nDev <diego-s.novaes@hotmail.com>
2021-08-17 23:31:13 -0300

    case happens an error in sale route, proccess will stop

--- functions/Buy.js
+++ functions/Buy.js
@@ -7,67 +7,78 @@ async function userhascoupon(userid, total, searchmethod, today, cart) {
   const usercoupon = JSON.parse(coupon);
   let getcoupon = usercoupon[0].code;
 
-  const codeid = await db.query(
-    "SELECT * from promotional_codes WHERE code = $1",
-    [getcoupon]
-  );
-  const postsaleheader = await db.query(
-    "INSERT INTO sale_header(user_id, promotional_code_id, total, payment_method_id, creation_user, update_user, deadline) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
-    [userid, codeid.rows[0].id, total, searchmethod.id, userid, userid, today]
-  );
-  await db.query(
-    "INSERT INTO bills_receive(user_id, sale_header_id, amount, method_id, deadline) VALUES($1, $2, $3, $4, $5) RETURNING *",
-    [userid, postsaleheader.rows[0].id, total, searchmethod.id, today]
-  );
-
-  cart.forEach(async (element) => {
-    await db.query(
-      "INSERT INTO sale_items (sale_header_id, line, product_id, delivery_method, sold_amount, unitary_value, discount_value, plus_value, total) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
-      [
-        postsaleheader.rows[0].id,
-        element.quantity,
-        element.product_id,
-        "Courier",
-        element.quantity,
-        element.unitary_price,
-        element.discount_value,
-        element.plus_value,
-        element.total_price,
-      ]
+  try {
+    const codeid = await db.query(
+      "SELECT * from promotional_codes WHERE code = $1",
+      [getcoupon]
     );
-  });
-  await fs.unlink(`././coupons/user${userid}.json`);
-  await fs.unlink(`././cart/cart${userid}.json`);
-  return "Payment successfully";
+    const postsaleheader = await db.query(
+      "INSERT INTO sale_header(user_id, promotional_code_id, total, payment_method_id, creation_user, update_user, deadline) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [userid, codeid.rows[0].id, total, searchmethod.id, userid, userid, today]
+    );
+    await db.query(
+      "INSERT INTO bills_receive(user_id, sale_header_id, amount, method_id, deadline) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      [userid, postsaleheader.rows[0].id, total, searchmethod.id, today]
+    );
+
+    cart.forEach(async (element) => {
+      await db.query(
+        "INSERT INTO sale_items (sale_header_id, line, product_id, delivery_method, sold_amount, unitary_value, discount_value, plus_value, total) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+        [
+          postsaleheader.rows[0].id,
+          element.quantity,
+          element.product_id,
+          "Courier",
+          element.quantity,
+          element.unitary_price,
+          element.discount_value,
+          element.plus_value,
+          element.total_price,
+        ]
+      );
+    });
+
+    await fs.unlink(`././coupons/user${userid}.json`);
+    await fs.unlink(`././cart/cart${userid}.json`);
+    return "Payment successfully";
+  } catch (err) {
+    console.log(err);
+    return "Error, please contact the administrator";
+  }
 }
 
 async function userhasnocoupon(userid, total, searchmethod, today, cart) {
-  const postsaleheader = await db.query(
-    "INSERT INTO sale_header(user_id, total, payment_method_id, creation_user, update_user, deadline) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-    [userid, total, searchmethod.id, userid, userid, today]
-  );
-  await db.query(
-    "INSERT INTO bills_receive(user_id, sale_header_id, amount, method_id, deadline) VALUES($1, $2, $3, $4, $5) RETURNING *",
-    [userid, postsaleheader.rows[0].id, total, searchmethod.id, today]
-  );
-  cart.forEach(async (element) => {
-    await db.query(
-      "INSERT INTO sale_items (sale_header_id, line, product_id, delivery_method, sold_amount, unitary_value, discount_value, plus_value, total) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
-      [
-        postsaleheader.rows[0].id,
-        element.quantity,
-        element.product_id,
-        "Courier",
-        element.quantity,
-        element.unitary_price,
-        element.discount_value,
-        element.plus_value,
-        element.total_price,
-      ]
+  try {
+    const postsaleheader = await db.query(
+      "INSERT INTO sale_header(user_id, total, payment_method_id, creation_user, update_user, deadline) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [userid, total, searchmethod.id, userid, userid, today]
     );
-  });
-  await fs.unlink(`././cart/cart${userid}.json`);
-  return "Payment successfully";
+    await db.query(
+      "INSERT INTO bills_receive(user_id, sale_header_id, amount, method_id, deadline) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      [userid, postsaleheader.rows[0].id, total, searchmethod.id, today]
+    );
+    cart.forEach(async (element) => {
+      await db.query(
+        "INSERT INTO sale_items (sale_header_id, line, product_id, delivery_method, sold_amount, unitary_value, discount_value, plus_value, total) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+        [
+          postsaleheader.rows[0].id,
+          element.quantity,
+          element.product_id,
+          "Courier",
+          element.quantity,
+          element.unitary_price,
+          element.discount_value,
+          element.plus_value,
+          element.total_price,
+        ]
+      );
+    });
+    await fs.unlink(`././cart/cart${userid}.json`);
+    return "Payment successfully";
+  } catch (err) {
+    console.log(err);
+    return "Error, please contact the administrator";
+  }
 }
 
 async function buy(today, userid, method) {
